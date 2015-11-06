e34b09b700e78339a654a3e4d196d3faf7d3e310
dvbelyaev <dvbelyaev@yandex.ru>
2015-11-06 19:29:12 +0300

    Plotting Assignment 1 for Exploratory Data Analysis

--- plot3.R
+++ plot3.R
@@ -46,9 +46,9 @@ lines(hpcData$DateTime,
 
 ## Making the legend
 legend("topright",
-       legend = c("Sub_metering_1    ",
-                  "Sub_metering_2    ",
-                  "Sub_metering_3    "),
+       legend = c("Sub_metering_1",
+                  "Sub_metering_2",
+                  "Sub_metering_3"),
        col = c("black", "red", "blue"),
        lty = 1)
 
